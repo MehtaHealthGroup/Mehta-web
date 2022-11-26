12bf46ad1ac7122d90c8a8e19311a38ce6c19ff9
Vikas-jangid <vikas.jangid@infobeans.com>
2022-11-26 15:35:23 +0530

    added routers for pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,10 @@ import Navbar from "./components/navbar";
 import Home from "./components/home";
 import About from "./components/about";
 import Contact from "./components/contact";
-import NotFound from "./components/notfound";
+import NotFound from "./components/notFound";
 import Footer from "./components/footer";
 import Payment from "./components/payment";
+import Product from "./components/product";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/product" element={<Product />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate replace to="/404" />} />
       </Routes>
