2195c9ddb48ccc5c6a1ba20a64c693fe72d07ab5
Vikas-jangid <vikas.jangid@infobeans.com>
2022-11-26 15:29:50 +0530

    project Setup Done

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import './index.css';
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import About from "./components/about";
+import Contact from "./components/contact";
+import NotFound from "./components/notfound";
+import Footer from "./components/footer";
+import Payment from "./components/payment";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>  
+    
+  );
+}
+
+export default App;
